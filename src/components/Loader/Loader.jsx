374094a5bb642d20900cba84e5cf46415185e315
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-05-15 14:38:07 +0300

    do without modal

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,12 @@
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '34913875-a3fb3e83b61c48d3709723712';
+import { Bars } from 'react-loader-spinner';
+import React from 'react';
 
-export const getSearchImg = images => {
-  return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${images}&image_type=photo&orientation=horizontal&per_page=12`
-  ).then(res => res.json());
+const Loader = () => {
+  return (
+    <div>
+      <Bars />
+    </div>
+  );
 };
+
+export default Loader;
