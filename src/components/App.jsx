0043404f1cc77a083f368d94f7007033fe49aba2
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-05-17 15:54:10 +0300

    corect fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,19 +17,24 @@ class App extends Component {
     page: 1,
     perPage: 12,
     modal: { url: '', tags: '' },
+    error: null,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const prevName = prevState.searchImg;
     const prevPage = prevState.page;
     const { page, perPage, hits, searchImg } = this.state;
+
     if (prevName !== searchImg || prevPage !== page) {
       this.setState({ isLoading: true, hits: null });
       getSearchImg(searchImg, page, perPage)
+        .then(res => {
+          if (res.ok) {
+            return res.json();
+          }
+          return Promise.reject(new Error('Did not find'));
+        })
         .then(data => {
-          // this.setState(prevState => ({
-          //   hits: null ? data.hits : [...prevState.hits, ...data.hits],
-          // }));
           if (hits === null) return this.setState({ hits: data.hits });
           return this.setState({ hits: [...prevState.hits, ...data.hits] });
         })
@@ -57,10 +62,12 @@ class App extends Component {
   };
 
   render() {
-    const { hits, isLoading, isShowModal, modal } = this.state;
+    const { hits, isLoading, isShowModal, modal, error, searchImg } =
+      this.state;
     return (
       <div className={css.App}>
         <ToastContainer />
+        {error && <h1>{error.message}</h1>}
         <Searchbar handleSearch={this.handleSearch} />
         {isLoading && <Loader />}
 
@@ -69,7 +76,9 @@ class App extends Component {
           <Button handleClick={this.handleAdd} title="Load more" />
         )}
         {hits?.length === 0 && (
-          <div style={{ fontSize: '24px', fontWeight: '600' }}>No results!</div>
+          <div style={{ fontSize: '24px', fontWeight: '600' }}>
+            No results {searchImg}!
+          </div>
         )}
         {isShowModal && <Modal onClose={this.closeModal} modal={modal} />}
       </div>
